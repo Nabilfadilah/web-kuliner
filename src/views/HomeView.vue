e51125c43c0e86f3ecdf8814adafbf948ed5c10e
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-28 11:43:44 +0700

    Memanggil hero component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,26 @@
   <div class="home">
     <!-- import navbar  -->
     <navbar-component /> 
-    <h1>Home</h1>
+    
+    <!-- import hero  -->
+     <div class="container">
+       <hero-component />
+     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import NavbarComponent from '@/components/NavbarComponent.vue'
+import HeroComponent from '@/components/HeroComponent.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    NavbarComponent
+    NavbarComponent,
+    HeroComponent
   }
 }
 </script>
+
+    HeroComponent
