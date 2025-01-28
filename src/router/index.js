241e3c8150b359831d36b122d54c4d7aaa0689ed
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-28 11:13:19 +0700

    Menambah route foods

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import FoodsView from '@/views/FoodsView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/foods',
+    name: 'Foods',
+    component: FoodsView
   },
   {
     path: '/about',
