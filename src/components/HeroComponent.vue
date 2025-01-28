1ee63e9cc94cc4bf6916571d751ec4ebc75e0d2b
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-28 11:43:07 +0700

    Membuat page hero dan panggil svg

---
+++ src/components/HeroComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="row">
+        <div class="col">
+            <div class="d-flex h-100">
+                <div class="justify-content-center align-self-center">
+                    <h2> <strong>Menu Makanan Enak,</strong><br> hanya dari Handphone</h2>
+                    <p>Ayo segera pilih dan pesan makanan favorit Anda</p>
+                    <button class="btn btn-sm btn-success"> <i class="bi bi-arrow-right"></i> Pesan</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <img src="../assets/images/hero.svg" width="100%">
+        </div>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    name: 'HeroComponent'
+}
+</script>
+
+<style>
+
+</style>
