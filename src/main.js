54abcfcdc9b6cdad89557588b549ab86c09fa155
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-28 10:38:33 +0700

    Style bootstrap css

--- src/main.js
+++ src/main.js
@@ -2,4 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// vue boostrap CLI BootstrapVue
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+import { BootstrapVue3 } from 'bootstrap-vue-3';
+
+const app = createApp(App);
+
+// Use the BootstrapVue3 plugin
+app.use(BootstrapVue3);
+
+// Use the Vue router
+app.use(router);
+
+// Mount the app
+app.mount('#app');
