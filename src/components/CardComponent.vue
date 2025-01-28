9f0e98ee75f39d98d1e81a87524759da4813ca24
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-28 15:22:36 +0700

    Membuat component card

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="card shadow-sm border-light rounded mb-4" style="max-width: 18rem; background-color: #f8f9fa;">
+      <!-- Gambar Produk -->
+      <img
+        :src="require('@/assets/images/' + product.gambar)"
+        class="card-img-top img-fluid"
+        alt="Product image"
+        style="height: 200px; object-fit: cover;"
+      />
+      
+      <div class="card-body">
+        <!-- Nama Produk -->
+        <h5 class="card-title font-weight-bold text-truncate" style="max-width: 100%;">{{ product.nama }}</h5>
+  
+        <!-- Harga Produk -->
+        <p class="card-text text-success font-weight-bold">
+          Rp. {{ product.harga }}
+        </p>
+  
+        <!-- Status Ketersediaan -->
+        <a href="#" class="btn btn-sm btn-success">
+          <i class="bi bi-bag"></i> {{ product.is_ready }}
+        </a>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "CardComponent",
+    props: ["product"],
+  };
+  </script>
+  
+  <style scoped>
+  .card-product {
+    border-radius: 10px;
+  }
+  
+  .card-body {
+    padding: 1.25rem;
+  }
+  
+  .card-img-top {
+    border-radius: 10px 10px 0 0;
+  }
+  </style>
+  
