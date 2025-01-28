b4563090900be8b2a6f93d17de6494f99265d697
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-28 20:07:38 +0700

    Membuat component food detail dan menjalankan fungsi get by id detail

---
+++ src/views/FoodDetailView.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="food-detail">
+    <navbar-component />
+    <div class="container">
+
+      <!-- breadcrumb -->
+      <div class="row mt-3">
+        <div class="col">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item">
+                <router-link to="/" class="text-dark">Home</router-link>
+              </li>
+              <li class="breadcrumb-item" aria-current="page">
+                <router-link to="/foods" class="text-dark">Foods</router-link>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">Food Order</li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+
+      <!-- content detail -->
+      <div class="row">
+        <div class="col-md-6">
+          <img v-if="product.gambar" :src="require('@/assets/images/' + product.gambar)" class="img-fluid shadow"
+            alt="Product Image">
+          <p v-else>Gambar tidak tersedia</p>
+        </div>
+        <div class="col-md-6">
+          <h4><strong>{{ product.nama }}</strong></h4>
+          <h4>Harga : <strong>Rp. {{ product.harga }}</strong></h4>
+          <form>
+            <div class="form-group">
+              <label for="jumlah_pemesanan">Jumlah Pesan</label>
+              <input type="number" class="form-control">
+            </div>
+            <div class="form-group">
+              <label for="keterangan">Keterangan</label>
+              <textarea class="form-control" placeholder="Keterangan sprt : Packing produk..."></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-success mt-3">
+              <i class="bi bi-bag"></i> Pesan
+            </button>
+          </form>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import NavbarComponent from "@/components/NavbarComponent.vue";
+import axios from "axios";
+
+export default {
+  name: "FoodDetail",
+  components: {
+    NavbarComponent,
+  },
+  // nantinya data API akan disimpan disini
+  data() {
+    return {
+      product: {}
+    };
+  },
+  methods: {
+    setProduct(data) {
+      this.product = data;
+    },
+  },
+  // ketika dijalankan/dipasang maka halaman code akan berjalan
+  mounted() {
+    // menggunakan axios
+    axios
+      .get("http://localhost:3000/products/" + this.$route.params.id)
+      .then((response) => {
+        // handle success
+        this.setProduct(response.data);
+        console.log("Berhasil GET data", response);
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+  }
+}
+
+</script>
+
+<style></style>
