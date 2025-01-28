4c4e76136833672870093a3492a0cecd63dfffb0
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-28 15:20:26 +0700

    Menambah component footer pada app

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
 <template>
   <router-view/>
+  <footer-component />
 </template>
+
+<script>
+import FooterComponent from "@/components/FooterComponent.vue";
+
+export default {
+  components: {
+    FooterComponent
+  }
+}
+
+
+</script>
