a5f83496b736e1cc8ec01293906cba796d5d76ff
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-28 10:39:14 +0700

    Import navbar component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- import navbar  -->
+    <navbar-component /> 
+    <h1>Home</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import NavbarComponent from '@/components/NavbarComponent.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    NavbarComponent
   }
 }
 </script>
