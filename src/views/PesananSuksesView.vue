c4372f00bf15cefddb9a09ecd50c56b8165a3db0
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-29 09:59:07 +0700

    Membuat view pemesanan sukses

---
+++ src/views/PesananSuksesView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="pesanan">
+    <navbar-component />
+    <div class="container">
+      <div class="row justify-content-center mt-5">
+        <div class="col text-center">
+          <img src="../assets/images/hero2.svg" width="300" />
+          <h2 class="mt-5"><strong>Sukses!!</strong></h2>
+          <h4>Pesanan Anda sedang diproses <br />Selamat Menunggu</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavbarComponent from "@/components/NavbarComponent.vue";
+
+export default {
+  name: "PesananSukses",
+  components: {
+    NavbarComponent,
+  },
+};
+</script>
+
+<style>
+</style>
