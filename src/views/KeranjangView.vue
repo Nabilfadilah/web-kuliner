2f59d1fbc1af81e983217b45b4fa282e20c0a971
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-29 07:33:10 +0700

    Membuat view keranjang

---
+++ src/views/KeranjangView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="keranjang">
+    <navbar-component />
+    <h2>Keranjang</h2>
+  </div>
+</template>
+
+<script>
+import NavbarComponent from "@/components/NavbarComponent.vue";
+
+export default {
+  name: "KeranjangView",
+  components: {
+    NavbarComponent,
+  },
+};
+</script>
+
+<style>
+</style>
