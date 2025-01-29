f11a116578b3b22750258d33350258e261f6137b
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-29 08:14:44 +0700

    Menambah table product dan mapping API get datanya

--- src/views/KeranjangView.vue
+++ src/views/KeranjangView.vue
@@ -1,17 +1,141 @@
 <template>
   <div class="keranjang">
     <navbar-component />
-    <h2>Keranjang</h2>
+
+    <div class="container">
+      <!-- breadcrumb -->
+      <div class="row mt-3">
+        <div class="col">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item">
+                <router-link to="/" class="text-dark">Home</router-link>
+              </li>
+              <li class="breadcrumb-item" aria-current="page">
+                <router-link to="/foods" class="text-dark">Foods</router-link>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">
+                Keranjang
+              </li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <h4>Keranjang <strong>Saya</strong></h4>
+          <div class="table-responsive mt-3">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">No.</th>
+                  <th scope="col">Gambar</th>
+                  <th scope="col">Makanan</th>
+                  <th scope="col">Keterangan</th>
+                  <th scope="col">Jumlah</th>
+                  <th scope="col">Harga</th>
+                  <th scope="col">Total Harga</th>
+                  <th scope="col">Hapus</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="(keranjang, index) in keranjangs"
+                  :key="keranjang.id"
+                >
+                  <th scope="row">{{ index + 1 }}.</th>
+                  <td>
+                    <img
+                      v-if="keranjang.products.gambar"
+                      :src="
+                        require('@/assets/images/' + keranjang.products.gambar)
+                      "
+                      class="img-fluid shadow"
+                      alt="Product Image"
+                    />
+                  </td>
+                  <td>
+                    <strong>{{ keranjang.products.nama }}</strong>
+                  </td>
+                  <td>
+                    {{ keranjang.keterangan ? keranjang.keterangan : "-" }}
+                  </td>
+                  <td>{{ keranjang.jumlah_pemesanan }}</td>
+                  <td>Rp. {{ keranjang.products.harga }}</td>
+                  <td>
+                    <strong
+                      >Rp.
+                      {{
+                        keranjang.products.harga * keranjang.jumlah_pemesanan
+                      }}</strong
+                    >
+                  </td>
+                  <td class="text-danger">
+                    <i class="bi bi-trash"></i>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td colspan="6" align="right">
+                    <strong>Total Harga : </strong>
+                  </td>
+                  <td>
+                    <strong>Rp. {{ totalHarga }}</strong>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import NavbarComponent from "@/components/NavbarComponent.vue";
+import axios from "axios";
 
 export default {
   name: "KeranjangView",
   components: {
     NavbarComponent,
+  },
+  data() {
+    return {
+      keranjangs: [],
+    };
+  },
+  methods: {
+    setKeranjang(data) {
+      this.keranjangs = data;
+    },
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/keranjangs")
+      .then((response) => {
+        // handle success
+        this.setKeranjang(response.data);
+        console.log("Berhasil GET data", response);
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+  },
+
+  // untuk menjumlahkan
+  computed: {
+    totalHarga() {
+      return this.keranjangs.reduce((total, item) => {
+        // return total + item.products.harga;
+        return (
+          total + (item.products?.harga || 0) * (item.jumlah_pemesanan || 0)
+        );
+      }, 0);
+    },
   },
 };
 </script>
